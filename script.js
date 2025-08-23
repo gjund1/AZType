14e6555e2d0a0913e3765d863ab7a884c0c1546b
JUND Guy <gjund@free.fr>
2025-08-23 07:34:24 +0200

    first add AZType

---
+++ script.js
@@ -0,0 +1,28 @@
+const listeMots = ["Cachalot", "Pétunia", "Serviette"]
+const listePhrases = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson"]
+
+let score = 0
+
+let choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
+
+while (choix !== "mots" && choix !== "phrases") {
+    choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
+}
+
+if (choix === "mots") {
+    for (let i = 0; i < listeMots.length; i++) {
+        let motUtilisateur = prompt("Entrez le mot : " + listeMots[i])
+        if (motUtilisateur === listeMots[i]) {
+            score++
+        }
+    }
+    console.log("Votre score est de " + score + " sur " + listeMots.length)
+} else {
+    for (let i = 0; i < listePhrases.length; i++) {
+        let phraseUtilisateur = prompt("Entrez la phrase : " + listePhrases[i])
+        if (phraseUtilisateur === listePhrases[i]) {
+            score++
+        }
+    }
+    console.log("Votre score est de " + score + " sur " + listePhrases.length)
+}
