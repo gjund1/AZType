316e7a512c9e1a69a90b597e641eb21794dc0cde
JUND Guy <gjund@free.fr>
2025-08-26 14:22:46 +0200

    AZType 3.1

--- scripts/script.js
+++ scripts/script.js
@@ -13,10 +13,8 @@ function choisirPhrasesOuMots() {
 function lancerBoucleDeJeu(listePropositions) {
     let score = 0
     for (let i = 0; i < listePropositions.length; i++) {
-        // On demande à l'utilisateur de saisir le mot correspondant à l'indice i
         let motUtilisateur = prompt("Entrez le mot : " + listePropositions[i])
         if (motUtilisateur === listePropositions[i]) {
-            // Si le mot saisi par l'utilisateur est correct, on incrémente le score
             score++
         }
     }
@@ -24,7 +22,6 @@ function lancerBoucleDeJeu(listePropositions) {
 }
 
 function lancerJeu() {
-    // Initialisations
     let choix = choisirPhrasesOuMots()
     let score = 0
     let nbMotsProposes = 0
