010b34bf1963d0134fb0c61f914a987901740d58
JUND Guy <gjund@free.fr>
2025-09-01 10:19:00 +0200

    AZType 4.1

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,8 @@
+/**
+ * Cette fonction affiche dans la console le score de l'utilisateur
+ * @param {number} score : le score de l'utilisateur
+ * @param {number} nbMotsProposes : le nombre de mots proposés à l'utilisateur
+ */
 function afficherResultat(score, nbMotsProposes) {
     let spanScore = document.querySelector(".zoneScore span")
     let affichageScore = `${score} / ${nbMotsProposes}` 
@@ -9,29 +14,50 @@ function afficherProposition(proposition) {
     zoneProposition.innerText = proposition
 }
 
+/**
+ * Cette fonction lance le jeu. 
+ * Elle demande à l'utilisateur de choisir entre "mots" et "phrases" et lance la boucle de jeu correspondante
+ */
 function lancerJeu() {
+    // Initialisations
     let score = 0
     let i = 0
+    let listeProposition = listeMots
 
     let btnValiderMot = document.getElementById("btnValiderMot")
     let inputEcriture = document.getElementById("inputEcriture")
-    afficherProposition(listeMots[i])
+
+    afficherProposition(listeProposition[i])
+
+    // Gestion de l'événement click sur le bouton "valider"
     btnValiderMot.addEventListener("click", () => {
-        console.log(inputEcriture.value)
-        if (inputEcriture.value === listeMots[i]) {
+        if (inputEcriture.value === listeProposition[i]) {
             score++
         }
         i++
         afficherResultat(score, i)
         inputEcriture.value = ''
-        if (listeMots[i] === undefined) {
+        if (listeProposition[i] === undefined) {
             afficherProposition("Le jeu est fini")
             btnValiderMot.disabled = true
         } else {
-            afficherProposition(listeMots[i])
+            afficherProposition(listeProposition[i])
         }
         
     })
+
+    // Gestion de l'événement change sur les boutons radios. 
+    let listeBtnRadio = document.querySelectorAll(".optionSource input")
+    for (let index = 0; index < listeBtnRadio.length; index++) {
+        listeBtnRadio[index].addEventListener("change", (event) => {
+            if (event.target.value === "1") {
+                listeProposition = listeMots
+            } else {
+                listeProposition = listePhrases
+            }
+            afficherProposition(listeProposition[i])
+        })
+    }
 
     afficherResultat(score, i)
 }
