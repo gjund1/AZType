6f86a15d85e312dd2bc71ac13325c5be58c78d29
JUND Guy <gjund@free.fr>
2025-09-01 10:15:50 +0200

    AZType 3.4

--- scripts/config.js
+++ scripts/config.js
@@ -1,2 +1,9 @@
+/*********************************************************************************
+ * 
+ * Ce fichier contient toutes les constantes nécessaires au fonctionnement du jeu.
+ * En particulier les listes de mots et de phrases proposés à l'utilisateur
+ * 
+ *********************************************************************************/
+
 const listeMots = ["Cachalot", "Pétunia", "Serviette"]
 const listePhrases = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson"]
